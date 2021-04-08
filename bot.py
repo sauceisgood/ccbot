6898f4a80e5ba031f20ba3d6e4927c7335dfd9af
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-09 01:32:57 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,81 @@
+# dashezup inspired
+import os
+from pytgcalls import GroupCall
+import ffmpeg
+from config import Config
+from pyrogram import filters, Client
+
+VOICE_CHATS = {}
+DEFAULT_DOWNLOAD_DIR = 'downloads/vcbot/'
+
+app = Client(STRING_SESSION, API_ID, API_HASH)
+
+@app.on_message(filters.command('play'))
+async def play_track(client, message):
+    if not message.reply_to_message or not message.reply_to_message.audio:
+        return
+    input_filename = os.path.join(
+        client.workdir, DEFAULT_DOWNLOAD_DIR,
+        'input.raw',
+    )
+    audio = message.reply_to_message.audio
+    audio_original = await message.reply_to_message.download()
+    await message.reply('Downloading...')
+    ffmpeg.input(audio_original).output(
+        input_filename,
+        format='s16le',
+        acodec='pcm_s16le',
+        ac=2, ar='48k',
+    ).overwrite_output().run()
+    os.remove(audio_original)
+    if VOICE_CHATS and message.chat.id in VOICE_CHATS:
+        text = f'Playing **{audio.title}**...'
+    else:
+        try:
+            group_call = GroupCall(client, input_filename)
+            await group_call.start(message.chat.id, False)
+        except RuntimeError:
+            await message.reply('Group Call doesnt exist')
+            return
+        VOICE_CHATS[message.chat.id] = group_call
+    await message.reply(text)
+
+
+@app.on_message(filters.command('stopvc'))
+async def stop_playing(_, message):
+    group_call = VOICE_CHATS[message.chat.id]
+    group_call.stop_playout()
+    os.remove('downloads/vcbot/input.raw')
+    await mesaage.reply('Stopped Playing...')
+
+
+@app.on_message(filters.command('joinvc'))
+async def join_voice_chat(client, message):
+    input_filename = os.path.join(
+        client.workdir, DEFAULT_DOWNLOAD_DIR,
+        'input.raw',
+    )
+    if message.chat.id in VOICE_CHATS:
+        await message.reply('Already joined to Voice Chat')
+        return
+    chat_id = message.chat.id
+    try:
+        group_call = GroupCall(client, input_filename)
+        await group_call.start(chat_id, False)
+    except RuntimeError:
+        await message.reply('lel error!')
+        return
+    VOICE_CHATS[chat_id] = group_call
+    await message.reply('Joined the Voice Chat')
+
+
+@app.on_message(filters.command('leavevc'))
+async def leave_voice_chat(client, message):
+    chat_id = message.chat.id
+    group_call = VOICE_CHATS[chat_id]
+    await group_call.stop()
+    VOICE_CHATS.pop(chat_id, None)
+    await message.reply('Left Voice Chat')
+
+app.start()
+print('>>> JEVC USERBOT STARTED')
