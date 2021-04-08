ce92cd61f6573312e7711725d44d00865b1c567f
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-09 01:38:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
-    APP_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
