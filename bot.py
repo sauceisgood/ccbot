93c1dd3486dd7dcad07bc0633b5ba031143b2102
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-12 23:18:33 +0530

    added deezer download

--- bot.py
+++ bot.py
@@ -9,9 +9,15 @@ from datetime import datetime
 from pyrogram import filters, Client, idle
 import requests
 import wget
+import aiohttp
+from random import randint
+import aiofiles
 
 VOICE_CHATS = {}
 DEFAULT_DOWNLOAD_DIR = 'downloads/vcbot/'
+
+# deezer download web of willum butcher bot
+ARQ = "https://thearq.tech/"
 
 api_id=Config.API_ID
 api_hash=Config.API_HASH
@@ -26,6 +32,16 @@ self_or_contact_filter = filters.create(
     message:
     (message.from_user and message.from_user.is_contact) or message.outgoing
 )
+
+# fetch url for deezer download
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except:
+                data = await resp.text()
+    return data
 
 # get args for saavn download
 def get_arg(message):
@@ -71,6 +87,40 @@ async def song(client, message):
     await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
     os.remove(ffile)
     await pak.delete()
+
+async def download_song(url):
+    song_name = f"{randint(6969, 6999)}.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(song_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return song_name
+
+# deezer download by willum butcher bot
+@app.on_message(filters.command("deezer"))
+async def deezer(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("What's the song you want 🧐")
+        return
+    text = message.text.split(None, 1)[1]
+    query = text.replace(" ", "%20")
+    hike = await message.reply_text("Searching...")
+    try:
+        r = await fetch(f"{ARQ}deezer?query={query}&count=1")
+        title = r[0]["title"]
+        url = r[0]["url"]
+        artist = r[0]["artist"]
+    except Exception as e:
+        await hike.edit(str(e))
+        return
+    await hike.edit("Downloading...")
+    song = await download_song(url)
+    await hike.edit("Uploading...")
+    await message.reply_audio(audio=song, title=title, performer=artist)
+    os.remove(song)
+    await hike.delete()
 
 @app.on_message(filters.command('play') & self_or_contact_filter)
 async def play_track(client, message):
