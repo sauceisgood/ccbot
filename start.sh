f5a9c61540d77b1a457a1aaab50f1fc756624a9e
sauceisgood <53635512+sauceisgood@users.noreply.github.com>
2021-09-27 22:03:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo..."
-git clone https://github.com/ImJanindu/VC_PLAYER.git /VC_PLAYER
+git clone https://github.com/sauceisgood/ccbot.git /ccbot
 cd /VC_PLAYER
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
